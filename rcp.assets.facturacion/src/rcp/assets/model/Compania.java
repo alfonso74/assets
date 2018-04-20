df3ff14dbee9b5f8a54ce40953be6843a65a4f40
alfonso74 <carlosal_p@yahoo.com>
2018-04-20 16:20:04 -0500

    Refactor menor para la definición del listado de compañías.

--- rcp.assets.facturacion/src/rcp/assets/model/Compania.java
+++ rcp.assets.facturacion/src/rcp/assets/model/Compania.java
@@ -24,6 +24,15 @@ public enum Compania {
 		return pagaItbms;
 	}
 	
+	public static String[] getCodigos() {
+		String[] codigos = new String[values().length];
+		int index = 0;
+		for(Compania v : values()) {
+			codigos[index++] = v.getCodigo();
+		}
+		return codigos;
+	}
+	
 	public static Compania from(String codigo) {
 		for(Compania v : values()) {
 			if (v.codigo.equals(codigo)) {
